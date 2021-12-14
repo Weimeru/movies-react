9eaa7d7e30d134dfcefab36cfe58fbab51042f42
Weimeru <60139917+Weimeru@users.noreply.github.com>
2021-12-13 22:23:36 -0500

    Creating Routes from home to Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { MoviesGrid } from "./MoviesGrid";
+import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+  } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails.jsx";
+import { LandingPage } from "./pages/LandingPage";
 
   export function App(){
     return (
-    <div>
+    <Router>
        
         <header>
-            <h1 className={styles.title}>
-                Movies
-            </h1>
+            <Link to="/">
+               <h1 className={styles.title}>Movies</h1>
+            </Link>
+            <br/>
+            <Link to="/movie">Movie</Link>
         </header>
 
         <main>
-            <MoviesGrid />
+            <Routes>
+                <Route exact path="/movie" element={< MovieDetails/>}/>
+                <Route path="/" element={<LandingPage />}/>
+            </Routes>
         </main>
 
-    </div>
+    </Router>
     );
 }
